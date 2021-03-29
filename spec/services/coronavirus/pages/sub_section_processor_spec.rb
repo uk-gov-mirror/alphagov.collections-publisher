ff1ba2883c2e7550c7d3c9b4d23c60fc840ba48d
Graham Lewis <gclssvglx@gmail.com>
2021-03-29 16:49:51 +0100

    Add action URL, content and summary to sub section
    Before publishing an article, content designers can create drafts of the proposed content. Sometimes, these drafts do not make it to the published stage and are discarded by the user.
    
    When this happens a new draft - from the original live content - needs to be created. To do this the publishing-api is called to get the current live version of the content. The draft discarder is then called which removes any existing sub sections from the current draft and recreates the sub sections based on the live version content.
    
    The draft discarder calls the sections presenter, which in turn calls the sub section processor. Ultimately, a data structure in the correct format is returned that contains the recreated title and sub sections in the format expected by UI.
    
    We need to update this call chain to include the new action_link_url, action_link_content and action_link_summary data (if present) and present that back to the UI for display in the action link view fields.
    
    There are three places that need updating to achieve this:
    
    - the sub section processor,
    
    - the sections presenter, and
    
    - the draft discarder
    
    This first change adds the action_link_content and action_link_summary fields to each sub section that is return by the sub section processor.
    
    In cases where the live version has a previously created featured_link, we need to construct the appropriate action link data within the sub section instead of the original featured_link markdown as previously.

--- spec/services/coronavirus/pages/sub_section_processor_spec.rb
+++ spec/services/coronavirus/pages/sub_section_processor_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 RSpec.describe Coronavirus::Pages::SubSectionProcessor do
   let(:title) { Faker::Lorem.sentence }
   let(:label) { Faker::Lorem.sentence }
+  let(:description) { Faker::Lorem.sentence }
   let(:url) { "/#{File.join(Faker::Lorem.words)}" }
   let(:label_1) { Faker::Lorem.sentence }
-  let(:url_1) { "/#{File.join(Faker::Lorem.words)}?priority-taxon=774cee22-d896-44c1-a611-e3109cce8eae" }
+  let(:url_1) { "/#{File.join(Faker::Lorem.words)}" }
   let(:data) do
     [
       {
@@ -15,7 +16,7 @@ RSpec.describe Coronavirus::Pages::SubSectionProcessor do
             "label" => label,
             "url" => url,
             "featured_link" => true,
-            "description" => Faker::Lorem.sentence,
+            "description" => description,
           },
           {
             "label" => label_1,
@@ -31,31 +32,45 @@ RSpec.describe Coronavirus::Pages::SubSectionProcessor do
     let(:lines) { subject[:content].split("\n") }
 
     it "creates the correct number of lines" do
-      # 3 = 1 title plus 2 links
-      expect(lines.count).to eq 3
+      # 2 = 1 title plus 1 link - feature_link's aren't created
+      expect(lines.count).to eq 2
     end
 
     it "has title as the first line" do
       expect(lines.first).to eq "####{title}"
     end
 
-    it "has the first link as the second line" do
-      expect(lines.second).to eq "[#{label}](#{url})"
+    it "has the first non-featured link as the second line" do
+      expect(lines.second).to eq "[#{label_1}](#{url_1})"
     end
 
-    it "removes any priority-taxons query parameters from the url" do
-      expect(lines.third).to eq "[#{label_1}](#{url_1.gsub('?priority-taxon=774cee22-d896-44c1-a611-e3109cce8eae', '')})"
+    it "stores the url, label and description in the appropriate action_link fields" do
+      expect(subject).to match hash_including(action_link_url: url, action_link_content: label, action_link_summary: description)
     end
 
-    it "returns the featured link" do
-      expect(subject[:action_link_url]).to eq(url)
+    context "with a priority_taxon" do
+      let(:url_1) { "/#{File.join(Faker::Lorem.words)}?priority-taxon=774cee22-d896-44c1-a611-e3109cce8eae" }
+
+      it "removes any priority-taxons query parameters from the url" do
+        expect(lines.second).to eq "[#{label_1}](#{url_1.gsub('?priority-taxon=774cee22-d896-44c1-a611-e3109cce8eae', '')})"
+      end
     end
 
     context "with blank title" do
-      let(:title) { "" }
+      let(:data) do
+        {
+          "title" => nil,
+          "list" => [
+            {
+              "label" => label_1,
+              "url" => url_1,
+            },
+          ],
+        }
+      end
 
       it "has the first link as its first line" do
-        expect(lines.first).to eq "[#{label}](#{url})"
+        expect(lines.first).to eq "[#{label_1}](#{url_1})"
       end
     end
   end
